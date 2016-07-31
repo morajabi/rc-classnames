af25b69a0921bb60dc03137cbc509a111084eac3
Mohammad Rajabifard <mo.rajbi@gmail.com>
2016-07-31 13:19:03 +0430

    Js Standard

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
+/*global define:true*/
+
 (function () {
+  var hasOwn = {}.hasOwnProperty
 
-  var hasOwn = {}.hasOwnProperty;
+  function ReactClassNames () {
+    var finalClassList = []
 
-  function ReactClassNames() {
-    var finalClassList = [];
+    // Iterate over arguments passed to function
     for (var i = 0; i < arguments.length; i++) {
-			var arg = arguments[i];
-      if (!arg) continue;
+      var arg = arguments[i]
+      // remove falsy value
+      if (!arg) continue
 
-      var typoOfArg = typeof arg;
+      var typoOfArg = typeof arg
 
       if (typoOfArg === 'string' || typoOfArg === 'number') {
-        finalClassList.push(arg);
+        // Arg passed is String or Number
+        finalClassList.push(arg)
       } else if (arg.constructor === Array) {
-        // Array
-        finalClassList.push(ReactClassNames.apply(null, arg));
+        // Arg passed is Array
+        finalClassList.push(ReactClassNames.apply(null, arg))
       } else if (arg !== null && typoOfArg === 'object') {
-        // Object
+        // Arg passed is Object
         for (var key in arg) {
-					if (hasOwn.call(arg, key) && arg[key]) {
-            finalClassList.push(key);
-					}
-				}
+          if (hasOwn.call(arg, key) && arg[key]) {
+            finalClassList.push(key)
+          }
+        }
       }
-
     }
 
-    return finalClassList.join(' ');
+    // Join classNames together
+    return finalClassList.join(' ')
   }
 
   if (typeof module !== 'undefined' && module.exports) {
-		module.exports = ReactClassNames;
-	} else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		define('classnames', [], function () {
-			return ReactClassNames;
-		});
-	} else {
-		window.ReactClassNames = ReactClassNames;
-	}
-
-}());
+    // Export as module
+    module.exports = ReactClassNames
+  } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    // Define for AMD
+    define('classnames', [], function () {
+      return ReactClassNames
+    })
+  } else {
+    // Defined as global function
+    window.ReactClassNames = ReactClassNames
+  }
+}())
